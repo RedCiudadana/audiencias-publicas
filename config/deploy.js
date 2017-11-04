fce792b76a486cb65d3a7b565cbdbf1fb14dd750
Hector Hurtarte <hectorh30@gmail.com>
2017-11-04 10:27:19 -0600

    Publicar última versión a audiencias-publicas.redciudadana.org

--- config/deploy.js
+++ config/deploy.js
@@ -6,25 +6,24 @@ module.exports = function(deployTarget) {
     // include other plugin configuration that applies to all deploy targets here
   };
 
-  if (deployTarget === 'development') {
-    ENV.build.environment = 'development';
-    // configure other plugins for development deploy target here
-  }
+  // if (deployTarget === 'development') {
+  //   ENV.build.environment = 'development';
+  //   // configure other plugins for development deploy target here
+  // }
 
-  if (deployTarget === 'staging') {
-    ENV.build.environment = 'production';
-    // configure other plugins for staging deploy target here
-  }
+  // if (deployTarget === 'staging') {
+  //   ENV.build.environment = 'production';
+  //   // configure other plugins for staging deploy target here
+  // }
 
   if (deployTarget === 'production') {
     ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
-  }
 
-  ENV.git = {
-    repo: 'https://github.com/Kosho969/presupuesto-abierto.git',
-    branch: 'gh-pages',
-  };
+    ENV.git = {
+      repo: 'https://github.com/RedCiudadana/audiencias-publicas.git',
+      branch: 'gh-pages',
+    };
+  }
 
   // Note: if you need to build some configuration asynchronously, you can return
   // a promise that resolves with the ENV object instead of returning the
